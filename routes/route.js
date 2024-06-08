26a82301aa35a48785c40a6e5cc6ad970c5f59af
muhammadshahrozshahzad <shahr@DESKTOP-CA89MHE>
2024-06-08 20:31:01 +0500

    updates

--- routes/route.js
+++ routes/route.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const router = express.Router();
-router.get('/',(req,res)=>{
+router.get('/signup',(req,res)=>{
     res.send('Hello world')
   
   })
