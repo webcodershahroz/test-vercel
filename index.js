469e60049e448b805857d61da510b5309ef6c4c2
muhammadshahrozshahzad <shahr@DESKTOP-CA89MHE>
2024-06-08 20:20:47 +0500

    update

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // server.mjs
 const express = require('express')
+const router = require('./routes/route')
 const app = express()
-app.get('/',(req,res)=>{
-  res.send('Hello world')
 
-})
+app.use('/',router)
 
-app.listen('3000',(req,res)=>{
-  console.log("app is listing on port http://localhost:3000")
+app.listen('4000',(req,res)=>{
+  console.log("app is listing on port http://localhost:4000")
 })
