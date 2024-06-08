61fcf05a4187dc15bd2f2d70590c6e96131e2318
muhammadshahrozshahzad <shahr@DESKTOP-CA89MHE>
2024-06-08 20:09:48 +0500

    server updated

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 // server.mjs
-const { createServer } =  require('http') 
+const express = require('express')
+const app = express()
+app.get('/',(req,res)=>{
+  res.send('Hello world')
 
-const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World!\n');
-});
+})
 
-// starts a simple http server locally on port 3000
-server.listen(3000, '127.0.0.1', () => {
-  console.log('Listening on 127.0.0.1:3000');
-});
+app.listen('3000',(req,res)=>{
+  console.log("app is listing on port http://localhost:3000")
+})
